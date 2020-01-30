63763ed0cbc8e21a55c5587e2262ed4e24e319e1
Edvin Ardö <ardo@kth.se>
2020-01-30 22:49:06 +0100

    feat/#1 Added JSON parser

---
+++ src/main/java/JSONParser.java
@@ -0,0 +1,83 @@
+import org.json.JSONObject;
+
+/**
+ * A JSON parser which can retrieve specific values from a GitHub webhook
+ */
+public class JSONParser {
+    private String jsonString;
+
+    /**
+     * Initializes a new JSONParser
+     *
+     * @param jsonString A JSON String
+     */
+    public JSONParser(String jsonString) {
+        this.jsonString = jsonString;
+    }
+
+    /**
+     * Retrieves the HTTPS clone URL from the JSON String
+     *
+     * @return The value under "repository": and then "clone_url": from the JSON String
+     */
+    public String getCloneURL() {
+        String name = getValue(new String[]{"repository", "clone_url"});
+        return removeSurroundingQuotes(name);
+    }
+
+    /**
+     * Retrieves the branch name from the JSON String
+     *
+     * @return Part of the value under "ref": from the JSON String
+     */
+    public String getBranchName() {
+        String name = getValue("ref");
+        name = removeSurroundingQuotes(name);
+        return name.substring(11);// remove the first part: "refs/heads/"
+    }
+
+    /**
+     * Retrieves the repository name from the JSON String
+     *
+     * @return The value under "repository": and then "name": from the JSON String
+     */
+    public String getRepoName() {
+        String name = getValue(new String[]{"repository", "name"});
+        return removeSurroundingQuotes(name);
+    }
+
+    /**
+     * Retrieves the value under all the keys in respective order from the JSON String
+     *
+     * @param keys The keys to the retrieved values in use order
+     * @return The value under all the keys from the JSON String
+     */
+    private String getValue(String[] keys) {
+        String temp = jsonString;
+        for (String key : keys) {
+            JSONObject object = new JSONObject(temp);
+            temp = JSONObject.valueToString(object.get(key));
+        }
+        return temp;
+    }
+
+    /**
+     * Retrieves the value under the key from the JSON String
+     *
+     * @param key The key to the retrieved value
+     * @return The value under the key from the JSON String
+     */
+    private String getValue(String key) {
+        return getValue(new String[]{key});
+    }
+
+    /**
+     * Removes the first and last char from a String
+     *
+     * @param string A String
+     * @return The String without the first and last char
+     */
+    private String removeSurroundingQuotes(String string) {
+        return string.substring(1, string.length() - 1);
+    }
+}
