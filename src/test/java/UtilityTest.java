8fd2fee5dabe59d563b04223f594edd19cc036ef
Edvin Ardö <ardo@kth.se>
2020-02-06 10:00:43 +0100

    test/#60 Created test for readToken

--- src/test/java/UtilityTest.java
+++ src/test/java/UtilityTest.java
@@ -1,8 +1,12 @@
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +20,7 @@ class UtilityTest {
     @Test
     public void testClearDirectory() {
         File dir = new File("test_build_history");
-        if(!dir.exists())
+        if (!dir.exists())
             dir.mkdir();
         File file = new File("test_build_history/test 20T03T20T30");
         File file1 = new File("test_build_history/test 20T01T20T30");
@@ -44,6 +48,36 @@ class UtilityTest {
         File[] files = dir.listFiles();
         if (files != null) {
             assertEquals(0, files.length);
+        }
+    }
+
+    @Test
+    @DisplayName("If token exists, test that it has a size > 0")
+    public void testReadTokenExisting() {
+        try {
+            Path newFilePath = Paths.get(".token");
+            Files.createFile(newFilePath);
+            String token = Utility.readToken();
+            assertTrue(token.length() > 0);
+        } catch (FileAlreadyExistsException ignored) {
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    @Test
+    @DisplayName("If token does not exist, create a new file and test that it is read correctly")
+    public void testReadTokenNew() {
+        try {
+            Path newFilePath = Paths.get(".token");
+            Files.createFile(newFilePath);
+            FileWriter writer = new FileWriter(".token");
+            writer.write("testToken");
+            writer.close();
+            BufferedReader reader = new BufferedReader(new FileReader(new File(".token")));
+            String token = reader.readLine();
+            assertEquals("testToken", token);
+        } catch (IOException ignored) {
         }
     }
 }
